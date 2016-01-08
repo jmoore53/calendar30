ba1e0d55fce5b614be1b19c401ef785475a51eb3
jmoore53 <jmoore6786wjm@hotmail.com>
2016-01-08 17:02:05 -0500

    Database schema and migrations updated

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20151215052351) do
 
-  create_table "lunch_times", force: :cascade do |t|
-    t.datetime "lunch_dat"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "firstN"
     t.string   "lastN"
